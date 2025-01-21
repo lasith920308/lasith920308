c1ed2bc1fd718dd457ac17da1b856d6efe6e008d
lasith920308 <fina202411@st.forum.ac.jp>
2025-01-21 14:09:50 +0900

    For testing pull from github

--- mycode.py
+++ mycode.py
@@ -6,3 +6,4 @@ print('changes for the second commit');
 print('changes for the third commit');
 print('changes for the fourth commit');
 print('changes for the fifth commit');
+print('The edit I am making directly from github account')
